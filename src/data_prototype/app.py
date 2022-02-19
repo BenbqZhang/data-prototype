6474e787fb624a226f18af8c0deb0d3079c97e74
BenbqZhang <ben.bq.zhang@gmail.com>
2022-02-19 20:39:51 +0800

    feat: add dropdown to annotate

--- src/data_prototype/app.py
+++ src/data_prototype/app.py
@@ -17,14 +17,27 @@ loc2_date = pd.date_range(start='2020-12-28 14:53:43.435', periods=LOC2_SAMPLE_N
 loc1_sensor_df = pd.DataFrame(np.random.randn(LOC1_SAMPLE_NUM, len(COLUMNS)), index=loc1_date, columns=COLUMNS)
 loc2_sensor_df = pd.DataFrame(np.random.randn(LOC2_SAMPLE_NUM, len(COLUMNS)), index=loc2_date, columns=COLUMNS)
 
+current_window_number = 1
+GROUND_LABELS=['normal', 'snow', 'ice']
+# annotation (window_number : label)
+annotated_labels = {}
+
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1('Dash App for Time-series-data Annotation', id='app-title', style={'textAlign':'center'}),
+    html.Hr(),
+
+    html.Div([
+        html.P('please select a label: '),
+        dcc.Dropdown(id='annotation-dropdown', options=GROUND_LABELS),
+    ]),
 
     dcc.Graph(id='loc1-sensor-graph'),
     dcc.Graph(id='loc2-sensor-graph'),
     dcc.Slider(id='slide-window-slider', min=1, max=10, step=1, value=1),
+
+    html.Div(id='hidden-div', style={'display' : 'none'}),
 ])
 
 def create_window_figure(window_df):
@@ -36,10 +49,21 @@ def create_window_figure(window_df):
         Input('slide-window-slider', 'value')
 )
 def udpate_slide_window(slider_number):
+    global current_window_number
+    current_window_number = slider_number
+
     start, end = (slider_number-1)*WINDOW_SIZE, slider_number*WINDOW_SIZE
     loc1_window_df = loc1_sensor_df[start:end]
     loc2_window_df = loc2_sensor_df[start:end]
     return create_window_figure(loc1_window_df), create_window_figure(loc2_window_df)
+
+@app.callback(
+        Output('hidden-div', 'children'),
+        Input('annotation-dropdown', 'value')
+)
+def update_current_window_label(label_value):
+    annotated_labels[current_window_number] = label_value
+    return ''
 
 if __name__ == '__main__':
     app.run_server(debug=True)
