190f80b71c47f8bfab34ea13762fbc5a5bd9c6de
BenbqZhang <ben.bq.zhang@gmail.com>
2022-02-22 16:13:37 +0800

    feat(app): get xaxis value

--- src/data_prototype/demo/sync.py
+++ src/data_prototype/demo/sync.py
@@ -9,6 +9,9 @@ from data import fakedata
 
 app = Dash(__name__)
 
+loc1_current_xaxis_start = 0
+loc2_current_xaxis_start = 0
+
 def create_figure(sensor_dataframe, time_index, window_size):
     """
     Create figure of fixed window-size by default.
@@ -24,9 +27,45 @@ def create_figure(sensor_dataframe, time_index, window_size):
 app.layout = html.Div([
     html.P("sensor location 1:"),
     html.Br(),
+    html.P(id="loc1-x-axis-info"),
+    html.Br(),
     dcc.Graph(id="loc1-sensor-graph", figure=create_figure(fakedata.loc1_sensor_df, fakedata.loc1_date, fakedata.WINDOW_SIZE)),
 
     html.P("sensor location 2:"),
     html.Br(),
+    html.P(id="loc2-x-axis-info"),
+    html.Br(),
     dcc.Graph(id="loc2-sensor-graph", figure=create_figure(fakedata.loc2_sensor_df, fakedata.loc2_date, fakedata.WINDOW_SIZE)),
+
+    html.Div(id='hidden-div', style={'display' : 'none'}),
+    html.Button(id='show-btn', children='show axis info'),
 ])
+
+@app.callback(
+        Output('hidden-div', 'children'),
+        Input('show-btn', 'n_clicks')
+)
+def show_info(n_clicks):
+    if n_clicks and n_clicks > 0:
+        print(loc1_current_xaxis_start, loc2_current_xaxis_start)
+
+@app.callback(
+        Output('loc1-x-axis-info', 'children'),
+        Input('loc1-sensor-graph', 'relayoutData')
+)
+def update_loc1_xaxis(relayoutData):
+    global loc1_current_xaxis_start
+    if relayoutData and 'xaxis.range[0]' in relayoutData:
+        loc1_current_xaxis_start = relayoutData['xaxis.range[0]']
+    return str(loc1_current_xaxis_start)
+
+@app.callback(
+        Output('loc2-x-axis-info', 'children'),
+        Input('loc2-sensor-graph', 'relayoutData')
+)
+def update_loc2_xaxis(relayoutData):
+    global loc2_current_xaxis_start
+    if relayoutData and 'xaxis.range[0]' in relayoutData:
+        loc2_current_xaxis_start = relayoutData['xaxis.range[0]']
+    return str(loc2_current_xaxis_start)
+
