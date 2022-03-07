f5b44c081937e269467f963779e40eb136abed91
BenbqZhang <ben.bq.zhang@gmail.com>
2022-03-07 11:42:53 +0800

    feat(app): implement annotation labels

--- src/data_prototype/demo/rangeannote.py
+++ src/data_prototype/demo/rangeannote.py
@@ -10,6 +10,15 @@ df = fakedata.loc1_sensor_df
 range_left, range_right = 0, 0
 annote_left, annote_right = 0, 0
 
+GROUND_LABELS=['normal', 'snow', 'ice']
+label_colors = {
+        'normal' : 'blue',
+        'snow' : 'red',
+        'ice' : 'green'
+}
+current_label = ''
+annotated_labels = []
+
 def create_figure(sensor_dataframe):
     fig = px.line(sensor_dataframe)
     fig.update_xaxes(
@@ -28,14 +37,19 @@ def create_figure(sensor_dataframe):
 
 app.layout = html.Div([
     html.Div([
+        dcc.Dropdown(id='annotation-dropdown', options=GROUND_LABELS),
         html.Button(id='left-btn', children='left'),
         html.Button(id='right-btn', children='right'),
         html.Button(id='submit-btn', children='submit'),
     ]),
     dcc.Graph(id="loc1-graph"),
     html.P(id='current-range'),
+    html.Div(id='hidden-div', style={'display' : 'none'}),
 ])
 
+def save_labels(file, labels):
+    # not really write to file temporarily
+    print(labels)
 
 @app.callback(
         Output('current-range', 'children'),
@@ -46,6 +60,16 @@ def update_range(relayoutData):
     if relayoutData and 'xaxis.range[0]' in relayoutData:
         range_left, range_right = relayoutData['xaxis.range[0]'], relayoutData['xaxis.range[1]']
     return f'{range_left}, {range_right}'
+
+@app.callback(
+        Output('hidden-div', 'children'),
+        Input('annotation-dropdown', 'value')
+)
+def update_current_label(value):
+    global current_label
+    if value is not None:
+        current_label = value
+    return ''
 
 left_clicks_old, right_clicks_old, submit_clicks_old = 0, 0, 0
 fig_backup = create_figure(df)
@@ -64,23 +88,27 @@ def update_annote(left_clicks, right_clicks, submit_clicks):
 
     if left_clicks and left_clicks > 0 and left_clicks != left_clicks_old:
         annote_left = range_left
+        lcolor = label_colors[current_label] if current_label in label_colors else 'black'
         fig.add_vline(x=annote_left, line_width=3, line_dash='dash',
-                line_color='green')
+                line_color=lcolor)
         fig.update_xaxes(range=[range_left, range_right])
         left_clicks_old = left_clicks
 
     if right_clicks and right_clicks > 0 and right_clicks != right_clicks_old:
         annote_right = range_right
+        lcolor = label_colors[current_label] if current_label in label_colors else 'black'
         fig.add_vline(x=annote_right, line_width=3, line_dash='dash',
-                line_color='red')
+                line_color=lcolor)
         fig.update_xaxes(range=[range_left, range_right])
         right_clicks_old = right_clicks
 
     if submit_clicks and submit_clicks > 0 and submit_clicks != submit_clicks_old:
-        print(annote_left, annote_right)
+        annotated_labels.append((annote_left, annote_right, current_label))
+        save_labels('labels.txt', annotated_labels[-1])
+        fcolor = label_colors[current_label] if current_label in label_colors else 'black'
         fig.add_vrect(x0=annote_left, x1=annote_right,
-                annotation_text='snow', annotation_position='top left',
-                fillcolor='green', opacity=0.25, line_width=0)
+                annotation_text=current_label, annotation_position='top left',
+                fillcolor=fcolor, opacity=0.25, line_width=0)
         fig.update_xaxes(range=[range_left, range_right])
         submit_clicks_old = submit_clicks
 
