03bf6c070fcb2e409ef8dd249cf253b9c50793e2
BenbqZhang <ben.bq.zhang@gmail.com>
2022-04-28 15:44:47 +0800

    formatted with black

--- src/data_prototype/demo/details.py
+++ src/data_prototype/demo/details.py
@@ -1,35 +1,31 @@
 from dash import Dash, html, Input, Output
 
 sensor_list = [
-        "sensor one",
-        "sensor two",
-        "sensor three",
-        "sensor five",
-        "sensor six",
-        "sensor seven"
-        ]
+    "sensor one",
+    "sensor two",
+    "sensor three",
+    "sensor five",
+    "sensor six",
+    "sensor seven",
+]
 
 app = Dash(__name__)
 
+
 def create_detail_component(summary_name, detail_children):
-    return html.Details(
-            children=[
-                html.Summary(summary_name),
-                detail_children
-                ]
-            )
+    return html.Details(children=[html.Summary(summary_name), detail_children])
+
 
 def generate_detail_list(alist):
     for name in alist:
-        detail_children = html.Div([
-            html.H2(f"{name}'s Title"),
-            html.Hr(),
-            html.P(f"{name}'s content")
-            ])
+        detail_children = html.Div(
+            [
+                html.H2(f"{name}'s Title"),
+                html.Hr(),
+                html.P(f"{name}'s content"),
+            ]
+        )
         yield create_detail_component(name, detail_children)
 
-app.layout = html.Div([
-    html.H1("Demo for details"),
-    *generate_detail_list(sensor_list)
-    ])
 
+app.layout = html.Div([html.H1("Demo for details"), *generate_detail_list(sensor_list)])
